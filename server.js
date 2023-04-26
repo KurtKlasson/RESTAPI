651e9d418359131a1221db92f2b1e9808d3126e0
Calle <derpninjas88rgmail.com>
2023-04-26 14:02:57 +0200

    main

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ var bodyParser = require("body-parser")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
+function hash(data) {
+  const hash = crypto.createHash('sha256');
+  hash.update(data);
+  return hash.digest('hex')
+ }
+ 
 app.use(bodyParser.json())
 
 var mysql = require('mysql');
@@ -44,15 +49,15 @@ app.get('/', (req, res) => {
         <li>GET /movies - Hämtar alla filmer</li>
         <li>POST /movie - Skapar en ny filmer/li>
         <li>POST /users - Skapar en ny användare/li>
-        <li>PUT /users/:id - Uppdaterar en befintlig användare med ID:t</li>
+        <li>PUT /movies/:title - Uppdaterar en befintlig film med titeln</li>
     </ul>
   `;
 
   res.send(routes);
 });
-// GET route for a specific movie
+
 app.get('/movie/:title', function(req, res) {
-  let sql = `SELECT * FROM movies WHERE title = '${req.body.title}'`;
+  let sql = `SELECT * FROM movies WHERE title = '${req.params.title}'`;
   connection.query(sql, function (err, result, fields) {
     if (err) throw err;
     console.log(result.length)
@@ -70,12 +75,6 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-
-app.get('/search', (req, res) => {
-  const query = req.query.q;
-  res.send(`Search query: ${query}`);
-  console.log(req.query)
-});
 
 app.post('/movie', function(req, res) {
   let sql = `Select * FROM movies where title = '${req.body.title}'`;
@@ -101,7 +100,6 @@ app.post('/movie', function(req, res) {
 app.put('/movies/:title', (req, res) => {
   const id = req.body.title;
   const updatedUserData = req.body.year;
-  
 
   connection.query('UPDATE movies SET year = ? WHERE title = ?', [updatedUserData, id], (error, results) => {
     if (error) {
@@ -109,7 +107,6 @@ app.put('/movies/:title', (req, res) => {
       res.status(400).send('Could not update movie.');
       return;
     }
-
    
     if (results.affectedRows === 0) {
       res.status(400).send('Movie not found.');
@@ -125,9 +122,7 @@ app.post('/users', (req, res) => {
   const password = req.body.password;
 
 
-  const salt = crypto.randomBytes(16).toString('hex');
-
-  const passwordHash = crypto.pbkdf2Sync(password, salt, 10000, 32, 'sha256').toString('hex');
+  const passwordHash = hash(password)
 
   connection.query('INSERT INTO users (username, password_hash) VALUES (?, ?)', [username, passwordHash], (err, result) => {
     if (err) throw err;
@@ -138,13 +133,36 @@ app.post('/users', (req, res) => {
 
 app.get('/user', function(req, res) {
   let searchQuery = req.query.q;
-  let sql = `SELECT * FROM users WHERE id = ${req.body.q} OR username = ${req.body.q}`;
+  console.log(searchQuery)
+  let sql = `SELECT * FROM users WHERE id = '${searchQuery}' OR username = '${searchQuery}'`;
   connection.query(sql, [searchQuery, searchQuery], function (err, results, fields) {
     if (err) throw err;
     if (results.length === 0) {
       res.status(404).send('Användaren hittades inte.');
     } else {
       res.send(results);
+    }
+  });
+});
+
+app.post('/login', function(req, res) {
+  let username = req.body.username;
+  let password = req.body.password;
+  let sql = `SELECT * FROM users WHERE username = '${username}'`;
+
+  connection.query(sql, function (err, results, fields) {
+    if (err) throw err;
+
+    if (results.length === 0) {
+      res.status(401).json({ message: "Invalid username or password" });
+    } else {
+      let user = results[0];
+      
+      if (user.password_hash === hash(password)) {
+        res.json({ message: "Login successful" });
+      } else {
+        res.status(401).json({ message: "Invalid username or password" });
+      }
     }
   });
 });
